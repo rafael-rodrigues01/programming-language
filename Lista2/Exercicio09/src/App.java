c0f40ff048be2c2fcaa96732d34b378c9f60c488
rafael-rodrigues01 <rafa_rodrigues01@yahoo.com>
2024-02-26 22:54:20 -0300

    Exercício 09 OK

---
+++ Lista2/Exercicio09/src/App.java
@@ -0,0 +1,38 @@
+/*
+9. Dados três valores, verificar se eles podem ser valores dos lados de um triângulo. Se forem, imprima se é um triângulo equilátero, isósceles ou escaleno.Condição de existência de um triângulo: a medida de qualquer um dos lados de um triângulo deve ser menor do que a soma dos outros dois lados e maior que diferença entre essas mesmas medidas.Triângulo equilátero: possui todos os lados iguais.Triângulo isósceles: possui apenas dois lados iguais.Triângulo escaleno: todos os lados são diferentes.
+ */
+
+import java.util.Scanner;
+
+public class App {
+  public static void main(String[] args) {
+    double firstSide, secondSide, thirdSide, sumOfBothSides;
+    boolean triaguloExiste;
+
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Digite o primeiro lado do triângulo:");
+    firstSide = in.nextDouble();
+
+    System.out.println("Digite o segundo lado do triângulo:");
+    secondSide = in.nextDouble();
+
+    System.out.println("Digite o terceiro lado do triângulo:");
+    thirdSide = in.nextDouble();
+
+    triaguloExiste = (firstSide + secondSide) > thirdSide && (firstSide + thirdSide) > secondSide && (secondSide + thirdSide) > firstSide;
+
+    if (triaguloExiste == true) {
+      if (firstSide == secondSide && secondSide == thirdSide && thirdSide == firstSide) {
+        System.out.println("Triângulo equilátero!");
+      } else if (firstSide == secondSide && secondSide == thirdSide || thirdSide == firstSide) {
+        System.out.println("Triângulo isósceles");
+      } else {
+        System.out.println("Triângulo escaleno");
+      }
+    } else {
+      System.out.println("Triângulo inexistente!");
+    }
+
+  }
+}
