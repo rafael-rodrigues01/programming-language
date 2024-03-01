8095c22187347291a646e1c5ed26556084295f5f
Rafael <rafa_rodrigues01@yahoo.com>
2024-02-29 23:56:58 -0300

    Exercício 01 OK

--- Lista3/Exercicio01/src/App.java
+++ Lista3/Exercicio01/src/App.java
@@ -1,5 +1,19 @@
+// 1. Criar um programa que receba um valor do usuário e exiba a sua tabuada multiplicandoo 
+// número digitado pela sequência de 0 a 10
+
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  public static void main(String[] args) throws Exception {
+    double value; 
+
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Digite um número:");
+    value = in.nextDouble();
+
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(value + " X " + i + " = " + value * i);
     }
+  }
 }
