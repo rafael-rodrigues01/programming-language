898c3b75b062e718b4371aed3b302af65c9a5564
rafael-rodrigues01 <rafa_rodrigues01@yahoo.com>
2024-03-06 13:41:41 -0300

    Exercício 06 OK

---
+++ Lista3/Exercicio06/src/App.java
@@ -0,0 +1,28 @@
+/*
+6. Peça para o usuário inserir dois números inteiros e exiba todos os números existentes
+entre os números digitados.
+ */
+
+import java.util.Scanner;
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    int number1, number2;
+
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("\nDigite um número inteiro:");
+    number1 = in.nextInt();
+
+    System.out.println("\nDigite o segundo número inteiro:");
+    number2 = in.nextInt();
+
+    System.out.println("\nNúmeros do intervalo:");
+
+    for (int i=number1+1; i<number2; i++){
+      System.out.println(i);
+    }
+
+    in.close();
+  }
+}
