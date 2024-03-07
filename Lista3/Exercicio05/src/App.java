eaab78c34a36452f82a0b8c96e87b68b3854c089
rafael-rodrigues01 <rafa_rodrigues01@yahoo.com>
2024-03-07 20:11:47 -0300

    Corrigido exercício 05

--- Lista3/Exercicio05/src/App.java
+++ Lista3/Exercicio05/src/App.java
@@ -6,20 +6,21 @@ import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) throws Exception {
-    double number, result;
-
+    int n,f;
+    f=1;
     Scanner in = new Scanner(System.in);
-
-    System.out.println("Digite um número:");
-    number = in.nextDouble();
-
-    result = 1;
-
-    for (int i = 1; i <= number; i++) {
-      result *= i;
-    }
-
-    System.out.println("Fatorial de " + number + " = " + result);
+    do {
+        System.out.println("Digite um numero: ");
+        n = in.nextInt();
+    }while(n<0);
+    
+    for(int i=n;i>1;i--)
+        f=f*i;
+    
+    //for(int i=1;i<=n;i++)
+        //f*=i;
+    
+    System.out.println(n+"!= "+f);
 
     in.close();
   }
