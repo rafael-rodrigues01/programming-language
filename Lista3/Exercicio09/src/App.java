bd90dd71d92e4dc8522469242e8fd483b10f82c1
rafael-rodrigues01 <rafa_rodrigues01@yahoo.com>
2024-03-07 22:10:21 -0300

    Corrigido exercício 09

--- Lista3/Exercicio09/src/App.java
+++ Lista3/Exercicio09/src/App.java
@@ -6,31 +6,30 @@ import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) throws Exception {
-    double number;
-
-    double result = 0;
-
+    int resposta, op;
+    resposta = 0;
     Scanner in = new Scanner(System.in);
-
-    for (int i = 1; i < 11; i++) {
-      System.out.println("\n" + "Tabuada do " + i);
-      for (int j = 1; j <= 10; j++) {
-        result = i * j;
-        System.out.println("Digite o resultado de " + i + " X " + j + " = " + " ? ");
-        number = in.nextDouble();
-
-        if (number == result) {
-          System.out.println(i + " X " + j + " = " + result);
-        } else {
-          System.out.println("Jogador perdeu ! (Jogar novamente)");
-          return;
+    do {
+      for (int i = 1; i <= 10; i++) {
+        int j;
+        for (j = 0; j <= 10; j++) {
+          System.out.println(i + "x" + j + "=");
+          resposta = in.nextInt();
+          if (resposta != i * j) {
+            System.out.println("Voce errou");
+            break;
+          }
         }
-
+        System.out.println(i + " " + j);
+        if (resposta != i * (j - 1))
+          break;
+        if (i == 10 && j == 10)
+          System.out.println("Parabens voce ganhou!");
       }
-    }
-
-    System.out.println("Parabéns você venceu ! Deseja jogar novamente ?");
-
+      System.out.println("Deseja jogar novamente?\n1.Sim\n2.Nao");
+      op = in.nextInt();
+    } while (op == 1);
+    System.out.println("Obrigado por jogar.");
     in.close();
   }
 }
