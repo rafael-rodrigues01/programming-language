09e31b5760773c4bab8cd8ff71115c1d9dcf2ab2
rafael-rodrigues01 <rafa_rodrigues01@yahoo.com>
2024-03-13 22:11:53 -0300

    Exercício 05 OK

--- Lista4/Exercicio05/src/App.java
+++ Lista4/Exercicio05/src/App.java
@@ -5,6 +5,18 @@
 
 public class App {
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
+    int [] vector = {10,20,30,40,50, 60,70,80,90};
+    int [] rotcev = new int[vector.length];
+    
+    for(int i=0;i< vector.length;i++){
+        rotcev[(vector.length-1)-i]=vector[i];
+    }
+    for (int n: vector) {
+        System.out.print(n + " ");
+    }
+    System.out.println();
+    for (int n: rotcev) {
+        System.out.print(n + " ");
+    }
   }
 }
