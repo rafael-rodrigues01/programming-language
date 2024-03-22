1c346f7ee0dd272f7b5268aa995276f215297d77
rafael-rodrigues01 <rafa_rodrigues01@yahoo.com>
2024-03-22 20:44:24 -0300

    Exercícios 08, 09, 10, 11, 12, Ok

--- Lista4/Exercicio08/src/App.java
+++ Lista4/Exercicio08/src/App.java
@@ -1,9 +1,18 @@
-/*
-8. Declare e inicialize uma matriz 5x5 em seguida exiba sua diagonal principal.
-*/
 
-public class App {
+// 8. Declare e inicialize uma matriz 5x5 em seguida exiba sua diagonal principal.
+
+public class Main {
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
+    int [][] matrix = {
+      { 1, 2, 3, 4, 5},
+      { 6, 7, 8, 9,10},
+      {11,12,13,14,15},
+      {16,17,18,19,20},
+      {21,22,23,24,25}};
+
+    for(int i = 0; i < 5; i++) {
+      System.out.print(matrix[i][i] + " ");
+    }
   }
+
 }
